6a3d16759e5440c7662d5ba4d218f5b2698c21fd
jSY49 <jseung49@gmail.com>
2026-09-03 15:08:17 +0900

    [design]about- skill layout 배치

---
+++ src/components/AboutSkill.tsx
@@ -0,0 +1,37 @@
+import { skillList } from "../data/skill/skills";
+import styles from "./AboutSkill.module.css";
+
+export default function AboutSkill() {
+	return (
+		<div className={styles.container}>
+			{skillList.map((skill) => {
+				const Icon = skill.icon;
+				return (
+					<div
+						className={styles.skills}
+						key={skill.type}
+					>
+						<Icon
+							className={styles.skill_image}
+							color={skill.values[0].color}
+							size={32}
+						/>
+						<h4 className={styles.skill_type}>{skill.type}</h4>
+						<ul className={styles.skill_list}>
+							{skill.values.map((value) => (
+								// TODO 스타일 적용
+								<li
+									className={styles.skill_item}
+									key={value.text}
+									style={{ color: value.color }}
+								>
+									{value.text}
+								</li>
+							))}
+						</ul>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
