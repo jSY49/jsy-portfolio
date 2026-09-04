54a00095b554810d07322ea8e3838ce462ad3ea5
jSY49 <jseung49@gmail.com>
2026-09-04 10:33:12 +0900

    [design] 헤더 디자인 수정

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import About from "../pages/About";
 import Career from "../pages/Career";
@@ -9,17 +9,47 @@ function App() {
 		<div className={styles.app}>
 			{/* header */}
 			<header className={styles.header}>
-				<h1 className={styles.header_title}> JSY's Portfolio</h1>
+				<div className={styles.header_left}>
+					<span className={styles.header_logo} />
+					<h1 className={styles.header_title}>JSY's Portfolio</h1>
+				</div>
 				<nav>
 					<ul className={styles.header_nav}>
 						<li className={styles.nav_item}>
-							<Link to={"/"}>About Me</Link>
+							<NavLink
+								to={"/"}
+								className={({ isActive }) =>
+									isActive
+										? `${styles.nav_link} ${styles.nav_link_active}`
+										: styles.nav_link
+								}
+							>
+								About Me
+							</NavLink>
 						</li>
 						<li className={styles.nav_item}>
-							<Link to={"/projects"}>Projects</Link>
+							<NavLink
+								to={"/projects"}
+								className={({ isActive }) =>
+									isActive
+										? `${styles.nav_link} ${styles.nav_link_active}`
+										: styles.nav_link
+								}
+							>
+								Projects
+							</NavLink>
 						</li>
 						<li className={styles.nav_item}>
-							<Link to={"/career"}>Career</Link>
+							<NavLink
+								to={"/career"}
+								className={({ isActive }) =>
+									isActive
+										? `${styles.nav_link} ${styles.nav_link_active}`
+										: styles.nav_link
+								}
+							>
+								Career
+							</NavLink>
 						</li>
 					</ul>
 				</nav>
