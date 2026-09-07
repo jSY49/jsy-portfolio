6a17a07580b80693870b041a148972b0039f18ca
jSY49 <jseung49@gmail.com>
2026-09-07 12:28:16 +0900

    [design] 페이지 전환 애니메이션 추가

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,13 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes, useLocation } from "react-router-dom";
 import styles from "./App.module.css";
 import About from "../pages/About";
 import Career from "../pages/Career";
 import Projects from "../pages/Projects";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+	const location = useLocation();
+
 	return (
 		<div className={styles.page}>
 			{/* header - 너비 확장 가능 하게 app 밖으로 분리 */}
@@ -60,22 +63,53 @@ function App() {
 
 			<div className={styles.app}>
 				{/* content */}
-				<Routes>
-					<Route
-						path="/"
-						element={<About />}
-					/>
-					<Route
-						path="/career"
-						element={<Career />}
-					/>
-					<Route
-						path="/projects"
-						element={<Projects />}
-					/>
-				</Routes>
+				{/* wait : 이전 페이지가 완전히 사라진 뒤 다음 페이지 나타나도록 함 (sync는 겹쳐서 ) */}
+				<AnimatePresence mode="wait">
+					<Routes
+						location={location}
+						key={location.pathname}
+					>
+						<Route
+							path="/"
+							element={
+								<PageWrapper>
+									<About />
+								</PageWrapper>
+							}
+						/>
+						<Route
+							path="/career"
+							element={
+								<PageWrapper>
+									<Career />
+								</PageWrapper>
+							}
+						/>
+						<Route
+							path="/projects"
+							element={
+								<PageWrapper>
+									<Projects />
+								</PageWrapper>
+							}
+						/>
+					</Routes>
+				</AnimatePresence>
 			</div>
 		</div>
+	);
+}
+
+function PageWrapper({ children }: { children: React.ReactNode }) {
+	return (
+		<motion.div
+			initial={{ opacity: 0, x: 12 }} //컴포넌트가 처음 마운트 될 때
+			animate={{ opacity: 1, x: 0 }} //컴포넌트가 최종 도달할 상태
+			exit={{ opacity: 0, x: -12 }} //컴포넌티가 언마운트 될 때
+			transition={{ duration: 0.25, ease: "easeInOut" }} //지속 시간 , 가속-감속 옵션
+		>
+			{children}
+		</motion.div>
 	);
 }
 
